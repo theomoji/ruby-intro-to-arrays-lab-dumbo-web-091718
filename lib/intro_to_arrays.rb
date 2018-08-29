6048d10ec2cf322564b9fb3e5d068ee41bbf81a9
Shuo Wang <gken90@gmail.com>
2018-08-29 22:20:16 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+
+
+def instantiate_new_array
+  my_new_array = []
+  my_new_array
+end
+
+def array_with_two_elements
+  new_array = [1, 2]
+  new_array
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  first_element(array)
+end
+
+def last_element_with_array_methods(array)
+  last_element(array)
+end
+
+def length_of_array(array)
+  array.length
+end
